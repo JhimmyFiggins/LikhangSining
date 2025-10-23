3dd5ad1501422afa4350e0a0630d5dba3ea0e7c7
Acrophyllix <acrophyllix@gmail.com>
2025-10-23 22:02:17 +0800

    Settings.java and Request.java functionality

--- src/Classes/Functions.java
+++ src/Classes/Functions.java
@@ -7,7 +7,7 @@ import javax.swing.JPanel;
 
 public class Functions {
     
-    public void MenuClicked(boolean HomeClicked, JPanel Home_BTN, boolean ArtistClicked, JPanel Artist_BTN)
+    public void MenuClicked(boolean HomeClicked, JPanel Home_BTN, boolean ArtistClicked, JPanel Artist_BTN, boolean RequestClicked, JPanel Request_BTN, boolean SettingsClicked, JPanel Settings_BTN)
     {
         if(HomeClicked == true)
         {
@@ -25,6 +25,24 @@ public class Functions {
         else 
         {
             Artist_BTN.setBackground(Color.yellow);
+        }
+        
+        if(RequestClicked == true)
+        {
+            Request_BTN.setBackground(Color.red);
+        }
+        else 
+        {
+            Request_BTN.setBackground(Color.yellow);
+        }
+        
+        if(SettingsClicked == true)
+        {
+            Settings_BTN.setBackground(Color.red);
+        }
+        else 
+        {
+            Settings_BTN.setBackground(Color.yellow);
         }
     }
 }
